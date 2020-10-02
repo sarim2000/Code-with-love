bc1d6851d1fde47737469be5f2a631a408b4707f
sarim2000 <50093514+sarim2000@users.noreply.github.com>
2020-10-02 20:04:22 +0530

    Create sarim2000_euclideangcdalgo.cpp

---
+++ cpp/sarim2000_euclideangcdalgo.cpp
@@ -0,0 +1,32 @@
+// C++ program to demonstrate Euclidean Algorithm 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Function to return 
+// gcd of a and b 
+int gcd(int a, int b) 
+{ 
+	if (a == 0) 
+		return b; 
+	return gcd(b % a, a); 
+} 
+
+// Driver Code 
+int main() 
+{ 
+	int a , b ;  cin >> a >> b;
+	cout << "GCD(" << a << ", "
+		<< b << ") = " << gcd(a, b) 
+						<< endl; 
+	a = 35, b = 10; 
+	cout << "GCD(" << a << ", "
+		<< b << ") = " << gcd(a, b) 
+						<< endl; 
+	a = 31, b = 2; 
+	cout << "GCD(" << a << ", "
+		<< b << ") = " << gcd(a, b) 
+						<< endl; 
+	return 0; 
+} 
+
+
